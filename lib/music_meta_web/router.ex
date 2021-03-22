853cc6246fb92e8f1862c31ed77bf3232acf05ae
J. Smeets <j.smeets@yipyip.nl>
2021-03-22 18:49:36 +0100

    Add elixir/phoenix docs, dev_docker to dashboard environments

--- lib/music_meta_web/router.ex
+++ lib/music_meta_web/router.ex
@@ -16,7 +16,7 @@ defmodule MusicMetaWeb.Router do
   # If your application does not have an admins-only section yet,
   # you can use Plug.BasicAuth to set up some basic authentication
   # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
+  if Mix.env() in [:dev, :dev_docker, :test] do
     import Phoenix.LiveDashboard.Router
 
     scope "/" do
